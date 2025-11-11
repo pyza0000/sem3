550c36943bb069859b9abd04305d62c05ff15606
Filip P <pyza171@gmail.com>
2025-11-11 02:18:05 +0100

    11/11/25r

--- lab04.py
+++ lab04.py
@@ -1,22 +1,32 @@
-from typing import List
-class Student:
-    def __init__ (self):
-        self.name = ""
-        self.last_name = ""
-        self.marks = []
-    def give_name(self, name: str, last_name: str) -> None:
-        self.name = name
-        self.last_name = last_name
-    def give_mark(self, mark: int) -> None:
-        self.marks.append(mark)
-    def get_marks(self) -> List[int]:
-        return self.marks
-    def say_hello(self) -> None:
-        print("Hello! I'm " + self.name + " " + self.last_name)
+# from typing import List
+# class Student:
+#     def __init__ (self):
+#         self.name = ""
+#         self.last_name = ""
+#         self.marks = []
+#     def give_name(self, name: str, last_name: str) -> None:
+#         self.name = name
+#         self.last_name = last_name
+#     def give_mark(self, mark: int) -> None:
+#         self.marks.append(mark)
+#     def get_marks(self) -> List[int]:
+#         return self.marks
+#     def say_hello(self) -> None:
+#         print("Hello! I'm " + self.name + " " + self.last_name)
+#     def getavg(self) -> float:
+#         return (sum(self.marks) / len(self.marks))
+#     def giveindex(self,idx: str) -> None:
+#         self.index = idx
+#
+# s = Student()
+# s.give_name("Jane", "Doe")
+# s.give_mark(5) # wywołanie sposób 1
+# Student.give_mark(s, 3)  # wywołanie sposób 2
+# print(s.get_marks())
+# s.giveindex("77")
+# s.say_hello()
+# print(f"średnia ocen {s.getavg()}")
 
-s = Student()
-s.give_name("Jane", "Doe")
-s.give_mark(5) # wywołanie sposób 1
-Student.give_mark(s, 3)  # wywołanie sposób 2
-print(s.get_marks())
-s.say_hello()
+#_---------------------------------------------------------------------------------------------------S
+class Vehicle:
+    def get
