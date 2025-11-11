ac39c6fc6322456c03c17280f41033a570c49437
pyza0000 <pyza171@gmail.com>
2025-11-11 20:21:47 +0100

    10/11/25r

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import pytest
 import math
 import random
 import sys
+
 def main():
-    a = float(input("Podaj liczbe zmiennoprzecinką: "))
-    print(f"{math.fabs(a)}")
+    pass
 if __name__ == "__main__":
     main()
 
